113836ff0f0d5cb2b0258ea73c89272fffe71039
limyel <limyel@outlook.com>
2023-01-10 17:20:31 +0800

    now history

---
+++ src/views/NowHistory.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="now-title">
+    我正在做/想些什么？
+  </div>
+
+  <div class="now-item">
+    <div class="now-time">
+      2023/1/6
+    </div>
+    <div class="now-content">
+      <li class="now-content-item">学习 netty，写一个内网穿透程序。学习 netty，写一个内网穿透程序。学习 netty，写一个内网穿透程序。学习 netty，写一个内网穿透程序。学习 netty，写一个内网穿透程序。学习 netty，写一个内网穿透程序。学习 netty，写一个内网穿透程序。</li>
+      <li class="now-content-item">学习 netty，写一个内网穿透程序。</li>
+      <li class="now-content-item">学习 netty，写一个内网穿透程序。</li>
+      <li class="now-content-item">学习 netty，写一个内网穿透程序。</li>
+    </div>
+  </div>
+
+  <div class="now-item">
+    <div class="now-time">
+      2023/1/6
+    </div>
+    <div class="now-content">
+      <li class="now-content-item">学习 netty，写一个内网穿透程序。学习 netty，写一个内网穿透程序。学习 netty，写一个内网穿透程序。学习 netty，写一个内网穿透程序。学习 netty，写一个内网穿透程序。学习 netty，写一个内网穿透程序。学习 netty，写一个内网穿透程序。</li>
+      <li class="now-content-item">学习 netty，写一个内网穿透程序。</li>
+      <li class="now-content-item">学习 netty，写一个内网穿透程序。</li>
+      <li class="now-content-item">学习 netty，写一个内网穿透程序。</li>
+    </div>
+  </div>
+
+  <div class="now-item">
+    <div class="now-time">
+      2023/1/6
+    </div>
+    <div class="now-content">
+      <li class="now-content-item">学习 netty，写一个内网穿透程序。学习 netty，写一个内网穿透程序。学习 netty，写一个内网穿透程序。学习 netty，写一个内网穿透程序。学习 netty，写一个内网穿透程序。学习 netty，写一个内网穿透程序。学习 netty，写一个内网穿透程序。</li>
+      <li class="now-content-item">学习 netty，写一个内网穿透程序。</li>
+      <li class="now-content-item">学习 netty，写一个内网穿透程序。</li>
+      <li class="now-content-item">学习 netty，写一个内网穿透程序。</li>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.now-title {
+  font-size: 1.5rem;
+  font-weight: 700;
+  margin-bottom: 20px;
+}
+
+.now-item {
+  .now-time {
+    font-weight: 900;
+  }
+
+  .now-content {
+    padding: 20px 50px 0;
+    margin-bottom: 50px;
+
+    .now-content-item {
+      margin-bottom: 20px;
+    }
+  }
+}
+
+</style>
